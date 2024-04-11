78e3933db12e6296318bf6ead68eceabf053f197
alersh <=>
2024-04-11 14:05:28 -0400

    Added a couple of functions and comments.

--- R/FileIO.R
+++ R/FileIO.R
@@ -104,12 +104,57 @@ fileIONet <- function(input, output, session, ns, network){
   shinyFileChoose(input, 'LoadFile', roots = c(wd = getwd()), filetypes = 'RDS', session = session)
 }
 
+#' Write R6 object into file
+#' @description Write an R6 object into a RDS file.
+#' @param obj The object to serialize.
+#' @param filename The name of the file
+#' @export
 writeR6 <- function(obj, filename = ""){
   saveRDS(obj$serialize(), file = filename)
 }
 
+#' Load a file containing the R6 object
+#' @description Load an R6 object from a RDS file.
+#' @param filename The name of the file to read
+#' @return The saved R6 object
+#' @export
 readR6 <- function(filename = "", obj){
   r <- readRDS(file = filename)
   obj$unserialize(r)
   return (obj)
 }
+
+#' Save the output data into a file.
+#' @description Save the output created by a node into a file.
+#' @param x The output data to be saved
+#' @param filename The name of the file
+#' @param repo The type of repository where the data will be saved.
+#' @param type The type of file to be saved
+#' @export
+saveOutput <- function(x, filename = NULL, repo = c("local"), type = c("RDS", "csv")){
+  if (repo == "local"){
+    dir.create("data", showWarnings = FALSE)
+    if (is.null(filename))
+      stop("Cannot save output because of missing filename.")
+    else{
+      saveRDS(x, file.path("data", filename))
+    }
+  }
+}
+
+#' Read output data from a file.
+#' @description  Load the output data from a file
+#' @param filename The name of the file
+#' @param repo The type of repository from which the data will be loaded
+#' @return The output data
+#' @export
+readOutput <- function(filename = NULL, repo = c("local")){
+  if (repo == "local"){
+    if (is.null(filename))
+      stop("Cannot read output file because of missing filename.")
+    else
+      return (readRDS(filename))
+  }
+}
+
+
